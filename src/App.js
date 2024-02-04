b6050cfe311b62c51ab7709ff11762a4d504edd1
Hyehyun1001 <hye22327@naver.com>
2024-02-04 23:59:34 +0900

    feat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
 import './App.css';
+import Edit from "./pages/Edit";
+import {Link, Route, Routes} from "react-router-dom";
+import React, { useReducer, useRef, useEffect, useState } from "react";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
 
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+const mockData = [
+    {id: "mock1",
+    date: new Date().getTime(),
+    content: "mock1",
+    emotionId: 1,
+    },
+    {id: "mock2",
+        date: new Date().getTime(),
+        content: "mock2", emotionId: 2,
+    },
+    { id: "mock3",
+        date: new Date().getTime(),
+        content: "mock3",
+        emotionId: 3,
+    },
+];
+function reducer(state, action) {
+    switch (action.type) {
+        case "INIT": {
+            return action.data;
+        }
+        case "CREATE": {
+            return [action.data, ...state];
+        }
+        case "UPDATE": {
+            return state.map((it) =>
+            String(it.id) === String(action.data.id) ? { ...action.data } : it
+            );
+        }
+        case "DELETE": {
+            return  state.filter((it) => String(it.id) !== String(action.targetId));
+        }
+        default: {
+            return state;
+        }
+    }
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isDataLoaded, setIsDataLoaded] = useState(false);
+    const [data, dispatch] = useReducer(reducer, []);
+    const idRef = useRef(0);
+
+    useEffect(() => {
+        dispatch({
+            type: "INIT",
+            data: mockData,
+        });
+        setIsDataLoaded(true);
+    }, []);
+    const onUpdate = (targetId, data, content, emotionId) => {
+        dispatch({
+            type: "UPDATE",
+            data: {
+                id: targetId,
+                data: new Date(date).getTime(),
+                content,
+                emotionId,
+            },
+        });
+    };
+    const onDelete = (targetId) => {
+        dispatch({
+            type: "DELETE",
+            targetId,
+        });
+    };
+    const onCreate = (date, content, emotionId) => {
+        dispatch({
+            type: "CREATE",
+            data: {
+                id: idRef.current,
+                date: new Date(date).getTime(),
+                content,
+                emotionId,
+            },
+        });
+        idRef.current += 1;
+    };
+
+    if (!isDataLoaded) {
+        return <div>데이터를 불러오는 중입니다</div>
+    } else  {
+        return (
+            <DiaryStateContext.Provider value={data}>
+                <DiaryDispatchContext.Provider
+                    value={{
+                        onCreate,
+                        onUpdate,
+                        onDelete,
+                    }}
+                >
+                    <div className="App">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/new" element={<new />} />
+                            <Route path="/diary/:id" element={<Diary />} />
+                            <Route path="/edit/:id" element={<Edit />} />
+                        </Routes>
+                    </div>
+                </DiaryDispatchContext.Provider>
+            </DiaryStateContext.Provider>
+        );
+    }
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,41 @@
+import { useState, useContext, useEffect } from "react";
+import Button from "../components/Button";
+import Header from "../components/Header";
+import {DiaryStateContext} from "../App";
+import { getMonthRangeByDate } from "../util";
+
+const Home = () => {
+    const data = useContext(DiaryStateContext);
+    const [pivotDate, setPivotDate] = useState(new Date());
+    const [filteredData, setFilteredData] = useState([]);
+    const headerTitle = `${pivotDate.getFullYear()}년 ${pivotDate.getMonth() +1}월`;
+    const onIncreaseMonth = () => {
+        setPiotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+    };
+    const  onDecreaseMonth = () => {
+        setPiotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+    };
+
+    useEffect(() => {
+        if (data.length >= 1) {
+            const { beginTimeStamp, endTimeStamp } = getMonthRangeByDate(pivotDate);
+            setFilteredData(
+                data.filter(
+                    (it) => beginTimeStamp <= it.date && it.date <= endTimeStamp
+                )
+            );
+        }else  {
+            setFilteredData([]);
+        }
+    }, [data, pivotDate]);
+    return (
+        <div>
+            <Header
+                title={headerTitle}
+                leftChild={<Button text={"<"} onClick={onDecreaseMonth} />}
+                rightChild={<Button text={">"} onClick={onIncreaseMonth} />}
+            />
+        </div>
+    );
+};
+export default Home
